e58d56c2a717959f7c1c3894c0bc1547204190ba
Andrey0800770 <andrey0800770@gmail.com>
2024-12-19 22:49:30 -0300

    fix error: member of anonymous union redeclares 'rcu'

--- include/linux/cred.h
+++ include/linux/cred.h
@@ -144,13 +144,13 @@ struct cred {
 	struct user_struct *user;	/* real user ID subscription */
 	struct user_namespace *user_ns; /* user_ns the caps and keyrings are relative to. */
 	struct group_info *group_info;	/* supplementary groups for euid/fsgid */
-	struct rcu_head	rcu;		/* RCU deletion hook */
-#ifdef CONFIG_RKP_KDP
-	atomic_t *use_cnt;
-	struct task_struct *bp_task;
-	void *bp_pgd;
-	unsigned long long type;
-#endif /*CONFIG_RKP_KDP*/
+
+	/* RCU deletion */
+	union {
+		int non_rcu;			/* Can we skip RCU deletion? */
+		struct rcu_head	rcu;		/* RCU deletion hook */
+	};
+
 };
 #ifdef CONFIG_RKP_KDP
 typedef struct cred_param{
